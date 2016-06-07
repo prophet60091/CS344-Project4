334ff6489dbc05ff7f23129fc35962c6ba364bd4
Robert <rpjackson@gmail.com>
2016-06-07 17:10:21 -0600

    initial commit of branch

--- otp_enc_d.c
+++ otp_enc_d.c
@@ -16,7 +16,7 @@ void error(char *msg, int severity)
     perror(msg);
 
     if (severity > 1){
-        exit(1);
+        exit(2);
     }
 
 }
@@ -66,39 +66,6 @@ int start_server(int port, int cc){
 // Encrypts the message based on the key provided
 // @ param the message to be encrpted
 // @ param the key (same format as above assumed to be at least length of msg)
-//char *_encrypt(char *msg, char *key){
-//    int i;
-//    int msgLength = (int)strlen(msg);
-//    int res;
-//    char * encMsg = calloc((size_t)msgLength, sizeof(char));
-//
-//    if(strlen(key) < msgLength){
-//        error("Invalid Key: too small", 1);
-//        return NULL;
-//    }
-//    for(i =0; i < msgLength; i++){
-//
-//        res = key[i] + msg[i]; // key ascii val + our ascii value
-//
-//        if( res > 126){     //take care of all printable ascii chars (wraps when outside of range)
-//            res = res - 126 + 32;
-//        }
-//
-//        //Checks for invalid characters (only "supposed" to allow A-Z )
-//        if((msg[i] > 90 || msg[i] < 65) && msg[i] != 32 ){
-//            fprintf(stdout, "Invalid Text!");
-//            return NULL;
-//        }
-//
-//        encMsg[i] = (char)res;
-//    }
-//    encMsg[i]= '\n';
-//
-//    return encMsg;
-//}
-// Encrypts the message based on the key provided
-// @ param the message to be encrpted
-// @ param the key (same format as above assumed to be at least length of msg)
 char *_encrypt(char *msg, char *key){
     int i;
     int msgLength = strlen(msg);
@@ -110,7 +77,6 @@ char *_encrypt(char *msg, char *key){
 
     if((msg[0] > 90 || msg[0] < 65) && msg[0] != 32 ){
         error("Invalid Text!", 1);
-        return 0;
     }
 
     for(i =0; i < msgLength; i++){
@@ -185,7 +151,7 @@ int _read_message(FILE *fpFILE, FILE *fpKEY, cryptog *msg){
     //gather the goods
     result = getline(&msg->msg, &s, fpFILE );
      if (result < 0){
-         error("failed reading from MSG file", 227);
+         error("failed reading from file", 227);
      }
 
     s= 0; //VIP!!! reset s to 0 for the reading the key
@@ -193,8 +159,8 @@ int _read_message(FILE *fpFILE, FILE *fpKEY, cryptog *msg){
     //get the key
     result = getline(&msg->key, &s, fpKEY );
     if (result < 0){
-        error("failed reading from KEY file", 227);
-        exit(1);
+        error("failed reading from file", 227);
+        exit(227);
     }
 
     //pop off the newline character
@@ -202,7 +168,7 @@ int _read_message(FILE *fpFILE, FILE *fpKEY, cryptog *msg){
     msg->key[strlen(msg->key) -1] = '\0';
 
     if(strlen(msg->key) < strlen(msg->msg) ){
-        //error("Key is too short for message!", 1);
+        error("Key is too short for message!", 1);
         fprintf(stdout,"Key is too short for message!");
 
     }
@@ -274,40 +240,18 @@ int sender(int socket, char *msg){
 
 }
 
-//checks the identity of the incoming program
-//@params the socket on which to send
-//@params pointer where the result will be stored
-int check_identity(int socket, char * incomingIdent){
+void check_identity(int socket, char * incomingIdent){
     int n=0;
-
     /// FIRST CHECK WHICH PROGRAM WANTS ACCESS
     if ((n = receiver(socket, incomingIdent, 3)) < 0){
         error("didnt receive IDENT", 2);
     }
 
-    if(strcmp(pgrmIDENT, incomingIdent) != 0) {
+    if(strcmp(pgrmIDENT, incomingIdent) != 0){
         error("unknown program trying to access this program", 1);
-        fprintf(stdout, "unknown program trying to access this program\n");
-
-        //break it off- write no
-        if ((n = write(socket, pgrmIDENT, 3)) < 3) {
-            fprintf(stdout, "only sent %i bytes", n);
-            error("Sending Port: Didn't send enough bytes", 1);
-        }
-
-        //shut things down:
-        //close the connection
-        close(socket);
-        return -1;
+        fprintf(stdout, "unknown program trying to access this program");
     }
 
-    //Tell them if they may proceed by echoing the program ident:
-    if ((n = write(socket, pgrmIDENT, 3)) < 3) {
-        fprintf(stdout, "only sent %i bytes", n);
-        error("Sending IDENT: Didn't send enough bytes", 1);
-    }
-
- return 0;
 }
 
 int main(int argc, char *argv[])
@@ -355,111 +299,107 @@ int main(int argc, char *argv[])
         memset(keyName, 0, sizeof(keyName));
 
         /// FIRST CHECK WHICH PROGRAM WANTS ACCESS
-        n= check_identity(accept_socket, incomingIdent);
+        check_identity(accept_socket, incomingIdent);
 
-        if(n == 0) { // may proceed
-            /// THEN ESTABLISH A NEW COMMUNICATION PORT
-            srand((unsigned) time(NULL)); // seed random
-            newPort = atoi(argv[1]) + (rand() % 4000 + 100); // newport starting point
+        /// THEN ESTABLISH A NEW COMMUNICATION PORT
+        srand((unsigned)time(NULL)); // seed random
+        newPort = atoi(argv[1]) + (rand() % 6000 + 1000); // newport starting point
 
-            //Loop unitl we get a good port
-            int i = 1;
-            while ((newSocket = start_server(newPort, 1)) < 0) {
+        //Loop unitl we get a good port
+        int i = 1;
+        while ((newSocket = start_server(newPort, 1)) < 0){
 
-                newPort = newPort + i; // base the new off of the last accepted FD (err socket descriptor)
-                i++;
-                //fprintf(stdout, "found a new port for ye...\n");
-            };
+            newPort = newPort +i; // base the new off of the last accepted FD (err socket descriptor)
+            i++;
+            //fprintf(stdout, "found a new port for ye...\n");
+        };
 
-            // gets the portnumber into a string
-            sprintf(newPortString, "%i", newPort);
+        sprintf(newPortString, "%i", newPort); // gets the portnumber into a string
+        //send that to the client
+        if( (n=write(accept_socket, newPortString, 8)) < 8){
+            fprintf(stdout, "only sent %i bytes", n);
+            error("Sending Port: Didn't send enough bytes", 1);
+        }else{
+            //fprintf(stdout, "told the clint to find me on port %i", newPort);
+        }
 
-            //send that to the client
-            if ((n = write(accept_socket, newPortString, 8)) < 8) {
-                fprintf(stdout, "only sent %i bytes", n);
-                error("Sending Port: Didn't send enough bytes", 1);
-            } else {
-                //fprintf(stdout, "told the clint to find me on port %i", newPort);
-            }
+        if( close(accept_socket) < 0)
+            error("closing accept socket", 1);
 
-            if (close(accept_socket) < 0)
-                error("closing accept socket", 1);
+        //FORK!!
+        pcessID = fork();
+        //printf("spawning processes..%i\n", pcessID);
+        //partially adapted from lecture 9 cs344
+        switch((int)pcessID){
 
-            //FORK!!
-            pcessID = fork();
-            //printf("spawning processes..%i\n", pcessID);
-            //partially adapted from lecture 9 cs344
-            switch ((int) pcessID) {
+            case -1:
+                //it's in a bad state
 
-                case -1:
-                    //it's in a bad state
-
-                    error("boom!", 5050505);
-                    exit(1);
+                error("boom!", 5050505);
+                exit(1);
 
 
-                case 0:// WE'RE IN THE CHILD PROCESS
+            case 0:// WE'RE IN THE CHILD PROCESS
 
-                    com_socket = accept(newSocket, (struct sockaddr *) &cli_addr, &clilen);
+                com_socket = accept(newSocket, (struct sockaddr *) &cli_addr, &clilen);
 
-                    if (com_socket < 0) {
-                        error("SERVER ERROR on Accept", 3);
-                    } else {
+                if (com_socket < 0) {
+                    error("SERVER ERROR on Accept", 3);
+                }else{
 
-                        //fprintf(stdout, "client connected on this shiny new socket!...\n");
-                    }
+                    //fprintf(stdout, "client connected on this shiny new socket!...\n");
+                }
 
-                    //immediately close the listening socket we don't want anyone else on it!
-                    if (close(newSocket) < 0)
-                        error("closing newSocket", 1);
+                //immediately close the listening socket we don't want anyone else on it!
+                if( close(newSocket) < 0)
+                    error("closing newSocket", 1);
 
-                    //NOW PROCESS MESSAGES LIKE
-                    if ((n = receiver(com_socket, fileName, 100)) < 0) {
-                        error("didnt receive file name", 2);
-                    }
+                //NOW PROCESS MESSAGES LIKE
+                if ((n = receiver(com_socket, fileName, 100)) < 0){
+                    error("didnt receive file name", 2);
+                }
 
-                    if ((n = receiver(com_socket, keyName, 100)) < 0) {
-                        error("didnt receive key file name", 2);
-                    }
+                if ((n= receiver(com_socket, keyName, 100)) < 0){
+                    error("didnt receive key file name", 2);
+                }
 
-                    if ((n = process_message(fileName, keyName, &encrypted)) < 0)
-                        error("couldn't process message", 1);
+                if(( n= process_message(fileName, keyName, &encrypted )) < 0)
+                    error("couldn't process message", 2);
 
-                    sprintf(eLength, "%zu", strlen(encrypted)); // gets the length of the encrypted txt into a string
+                sprintf(eLength, "%zu", strlen(encrypted)); // gets the length of the encrypted txt into a string
 
-                    if ((n = write(com_socket, eLength, 8)) < 8) {
-                        fprintf(stdout, "only sent %i bytes", n);
-                        error("Writing Size: Didn't send enough bytes", 1);
-                    }
+                if( (n=write(com_socket, eLength, 8)) < 8){
+                    fprintf(stdout, "only sent %i bytes", n);
+                    error("Writing Size: Didn't send enough bytes", 1);
+                }
 
-                    if (strlen(encrypted) != 0) {  // The file must contain data
-                        if ((n = sender(com_socket, encrypted)) < 0) {
-                            error("Failed Sending", 1);
-                        }
-                    }
+                if ((n = sender(com_socket, encrypted)) < 0){
+                    error("Failed Sending", 1);
+                }
 
-                    if (close(com_socket) < 0)
-                        error("closing com socket", 1);
+                if( close(com_socket) < 0)
+                    error("closing com socket", 1);
 
-                    free(encrypted);
+                free (encrypted);
 
-                    exit(0); // make sure the process is terminated
+                exit(0); // make sure the process is terminated
 
-                default:
-                    // WERE IN THE PARENT
-                    //ADAPTED FROM **http://brennan.io/2015/01/16/write-a-shell-in-c/**//
-                    //Too elegant to pass up. it works really well
-                    //  were storing the result of waitpid using WUNTRACED, (reports its status whether stopped or not)
-                    // as long as the process didn't exit, or receive a signal, so it's waiting util that happens
-                    // when it does, we know that the child process is complete.
+            default:
+                // WERE IN THE PARENT
+                //ADAPTED FROM **http://brennan.io/2015/01/16/write-a-shell-in-c/**//
+                //Too elegant to pass up. it works really well
+                //  were storing the result of waitpid using WUNTRACED, (reports its status whether stopped or not)
+                // as long as the process didn't exit, or receive a signal, so it's waiting util that happens
+                // when it does, we know that the child process is complete.
                     do {
                         wpid = waitpid(pcessID, &status, WUNTRACED);
 
                     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 
 
-            }
         }
+
+
     }
 
     if( close(socket) < 0)
